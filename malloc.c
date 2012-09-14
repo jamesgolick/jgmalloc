b5066b45290a9b4625031991cf7f899536298e35
James Golick <jamesgolick@gmail.com>
2012-09-13 17:06:06 -0700

    fuck everything. time to try again.

--- malloc.c
+++ malloc.c
@@ -22,93 +22,14 @@ int allocations = 0;
 int frees = 0;
 int totalBytesAllocated = 0;
 
-pthread_mutex_t heapLock = PTHREAD_MUTEX_INITIALIZER;
-
-struct malloc_chunk *freeListStart;
-
 void* malloc(size_t block_size) {
   fprintf(stderr, "enter malloc\n");
 
-  pthread_mutex_lock(&heapLock);
-  if (freeListStart != NULL) {
-    struct malloc_chunk *chunk = freeListStart;
-
-    do {
-      if (chunk->size >= block_size) {
-
-#ifdef DEBUG
-	print_malloc_stats();
-#endif
-
-	if (chunk->prev != NULL)
-	  chunk->prev->next = chunk->next;
-
-	if (chunk->next != NULL)
-	  chunk->next->prev = chunk->prev;
-
-	if (chunk == freeListStart)
-	  freeListStart = chunk->next;
-
-	chunk->prev = NULL;
-	chunk->next = NULL;
-
-	fprintf(stderr, "reissuing %p chunk %p\n", chunk + 1, chunk);
-
-	pthread_mutex_unlock(&heapLock);
-	return chunk + 1;
-      }
-    } while((chunk = chunk->next) != NULL);
-  }
-  pthread_mutex_unlock(&heapLock);
-
-  allocations++;
-
-  size_t size = block_size + sizeof(struct malloc_chunk);
-  if (size % ALIGN_TO > 0)
-    size += ALIGN_TO - (size % ALIGN_TO);
-
-  assert(size % ALIGN_TO == 0);
-
-  totalBytesAllocated += size;
-
-  struct malloc_chunk *ptr = sbrk(size);
-  if (ptr == -1) return NULL;
-  ptr->magic = 8;
-  ptr->size = block_size;
-
-#ifdef DEBUG
-  print_malloc_stats();
-#endif
-  fprintf(stderr, "issuing pointer %p chunk %p\n", ptr + 1, ptr);
-
-  return ptr + 1;
+  return NULL;
 }
 
 void free(void* ptr) {
   if (ptr == NULL) return;
-
-  pthread_mutex_lock(&heapLock);
-  struct malloc_chunk *chunk = ((struct malloc_chunk *)ptr) - 1;
-  fprintf(stderr, "enter free with %p chunk %p\n", ptr, chunk);
-
-  if (freeListStart == NULL) {
-    freeListStart = chunk;
-    frees++;
-  } else {
-    struct malloc_chunk *last = freeListStart;
-
-    while(last->next != NULL) {
-      last = last->next;
-    }
-
-    last->next = chunk;
-    chunk->prev = last;
-
-    frees++;
-  }
-
-  pthread_mutex_unlock(&heapLock);
-  print_malloc_stats();
 }
 
 void print_malloc_stats() {
@@ -121,7 +42,8 @@ void *realloc(void *ptr, size_t size) {
   void *newPtr = malloc(size);
 
   if (ptr != NULL) {
-    memcpy(newPtr, ptr, sizeof(ptr));
+    if (newPtr != NULL)
+      memcpy(newPtr, ptr, sizeof(ptr));
     free(ptr);
   }
 
@@ -133,7 +55,8 @@ void *realloc(void *ptr, size_t size) {
 void *calloc(size_t count, size_t size) {
   fprintf(stderr, "entering calloc.\n");
   void *ptr = malloc(count * size);
-  memset(ptr, 0, count * size);
+  if (ptr != NULL)
+    memset(ptr, 0, count * size);
   fprintf(stderr, "calloc returning pointer %p\n", ptr);
   return ptr;
 }
